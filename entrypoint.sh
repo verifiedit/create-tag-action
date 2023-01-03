98fcfa0f17a196ac4273f90e2ecdf4705cfd509d
Dean Tedesco <dean@verified.com.au>
2023-01-03 14:20:57 +1100

    fix: make /github/workspace a saf directory according to git

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -eu
 
-# Set up .netrc file with GitHub credentials
 git_setup ( ) {
     git config --global user.email "actions@github.com"
     git config --global user.name "Create Tag Action"
+    git config --global --add safe.directory /github/workspace
 }
 
 echo "Creating tag $INPUT_TAG with message '$INPUT_MESSAGE' as '$GITHUB_ACTOR'"
